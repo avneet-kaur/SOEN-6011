30fb23b2ce4de08284cc01de08677af9925feb43
avneet-kaur <avneetpannu@Avneets-MacBook-Air.local>
2022-08-04 23:03:05 -0400

    adding functionality to calculate natural exponential function

--- src/ScientificCalculator.java
+++ src/ScientificCalculator.java
@@ -1,18 +1,51 @@
-import java.util.Scanner;
-
 public class ScientificCalculator {
 
+    public static double naturalExponential(int x) {
+        //calculate e^x
+        int n_terms = 25;
+        double expo_sum = 1;
+        for (int i = n_terms - 1; i > 0; --i)
+            expo_sum = 1 + x * expo_sum / i;
+        return expo_sum;
+    }
+
+    public static double calculatePower(double x, int n) {
+        if (n < 0) {
+//			if n is -ve
+            return 1.0 / powHelper(x, Math.abs(n));
+        }
+//		n is +ve
+        return powHelper(x, n);
+    }
+
+    public static double powHelper(double x, int n) {
+//		recursive logic for power
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if (n % 2 == 0)
+            return powHelper(x * x, n / 2);
+        return x * powHelper(x * x, n / 2);
+    }
+
     public static String calculateExponentialFunction(String a, String b, String x) {
+        double res;
+        double t_a = 0, t_b = 0;
+        int t_x = 0;
+        t_a = Long.parseLong(a);
 
-        // if x==0
-
-        //1. a*e^x
-
-        //  a*10^x
-
-        // a * b^x
-
-        return null;
-
+        if (b.equals("e")) {
+            res = naturalExponential(Integer.parseInt(x));
+            res = t_a * res;
+        }
+//        else {
+//            t_b = Double.parseDouble(b);
+//            t_x = Integer.parseInt(x);
+//            res = calculatePower(t_b, t_x);
+//            res = t_a * res;
+//        }
+        res=0;
+//		Result -> a*b^x
+        System.out.println("Result = " + res + "\n");
+        return Double.toString(res);
     }
 }
