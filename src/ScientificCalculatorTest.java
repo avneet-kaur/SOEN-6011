ad1219605baf6a717de22e4b6828a0d32314847b
avneet-kaur <avneetpannu@Avneets-MacBook-Air.local>
2022-08-05 00:59:23 -0400

    adding junit test case file

---
+++ src/ScientificCalculatorTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScientificCalculatorTest {
+
+    @Test
+    public void naturalExponential() {
+    }
+
+    @Test
+    public void calculatePower() {
+    }
+
+    @Test
+    public void powHelper() {
+    }
+
+    @Test
+    public void calculateExponentialFunction() {
+    }
+}
